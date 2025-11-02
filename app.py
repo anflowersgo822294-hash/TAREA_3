025e351baef6e066538ad78fee54e4ed459a0f51
anflowersgo822294-hash <anflowersgo822294@gmail.com>
2025-11-02 17:35:37 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,23 +3,25 @@ from modulos.venta import mostrar_venta
 from modulos.login import login
 from modulos.productos import mostrar_productos
 from modulos.inicio import mostrar_inicio
+from modulos.clientes import mostrar_clientes, registrar_cliente
 
-# Verificar si la sesión está iniciada
-if st.session_state.get("sesion_iniciada", False):
-    # Mostrar saludo personalizado si el nombre está disponible
+def mostrar_menu():
     nombre = st.session_state.get("nombre_cliente", "Cliente")
     st.sidebar.markdown(f"👤 **Bienvenido, {nombre}**")
 
-    # Menú lateral
-    seleccion = st.sidebar.selectbox("Selecciona una opción", ["Inicio", "Ventas", "Productos"])
+    seleccion = st.sidebar.selectbox("Selecciona una opción", ["Inicio", "Ventas", "Productos", "Clientes"])
 
-    # Mostrar contenido según la opción seleccionada
     if seleccion == "Inicio":
         mostrar_inicio()
     elif seleccion == "Ventas":
         mostrar_venta()
     elif seleccion == "Productos":
         mostrar_productos()
+    elif seleccion == "Clientes":
+        mostrar_clientes()
+        registrar_cliente()
+
+if st.session_state.get("sesion_iniciada", False):
+    mostrar_menu()
 else:
-    # Mostrar pantalla de login si no hay sesión activa
     login()
