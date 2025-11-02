1e433562e22151100ec982a5710adfa85f665eda
anflowersgo822294-hash <anflowersgo822294@gmail.com>
2025-11-02 17:49:54 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ from modulos.venta import mostrar_venta
 from modulos.login import login
 from modulos.productos import mostrar_productos
 from modulos.inicio import mostrar_inicio
+from modulos.clientes import mostrar_clientes, registrar_cliente
 
 def mostrar_menu():
     nombre = st.session_state.get("nombre_cliente", "Cliente")
     st.sidebar.markdown(f"👤 **Bienvenido, {nombre}**")
 
-    seleccion = st.sidebar.selectbox("Selecciona una opción", ["Inicio", "Ventas", "Productos"])
+    seleccion = st.sidebar.selectbox("Selecciona una opción", ["Inicio", "Ventas", "Productos", "Clientes"])
 
     if seleccion == "Inicio":
         mostrar_inicio()
@@ -16,6 +17,9 @@ def mostrar_menu():
         mostrar_venta()
     elif seleccion == "Productos":
         mostrar_productos()
+    elif seleccion == "Clientes":
+        mostrar_clientes()
+        registrar_cliente()
 
 if st.session_state.get("sesion_iniciada", False):
     mostrar_menu()
