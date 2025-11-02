b0b62fc83010e49e8b2493706be34cc70265b6e8
anflowersgo822294-hash <anflowersgo822294@gmail.com>
2025-11-02 16:37:39 -0600

    Update inicio.py

--- modulos/inicio.py
+++ modulos/inicio.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-def mostrar_venta():
+def mostrar_inicio():
     # Aquí se muestra "Hola Mundo"
     st.title("Bienvenidos")
     st.write("Encuentra la información que necesitas aquí")
