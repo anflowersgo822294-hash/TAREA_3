2e6201d89929da6595240b5b0ef0cdbc7e107efd
00126222-dev <00126222@uca.edu.sv>
2025-11-01 22:16:08 -0600

    Update login.py

--- modulos/login.py
+++ modulos/login.py
@@ -2,7 +2,7 @@ import streamlit as st
 from modulos.config.conexion import obtener_conexion
 
 
-def verificar_usuario(usuario, contraseña):
+def verificar_usuario(Usuario, Contraseña):
     con = obtener_conexion()
     if not con:
         st.error("⚠️ No se pudo conectar a la base de datos.")
